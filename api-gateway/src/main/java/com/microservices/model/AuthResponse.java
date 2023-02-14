dc491645bbf330987aed5fd565209d9c5b5ae252
Gaurab Sah <gaurab.sah.777@gmail.com>
2023-02-14 18:23:40 +0530

    added security with OKTA

---
+++ api-gateway/src/main/java/com/microservices/model/AuthResponse.java
@@ -0,0 +1,26 @@
+package com.microservices.model;
+
+import java.util.Collection;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthResponse {
+
+	private String userId;
+
+	private String accessToken;
+
+	private String refreshToken;
+
+	private long expireAt;
+
+	private Collection<String> authorities;
+
+}
